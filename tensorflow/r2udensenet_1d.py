467703d1d71f7ed2360ed4702ccfb1f6d98f0a7f
Henry Salkever <henry.salkever@icloud.com>
2024-06-28 21:43:18 +0000

    created tensorflow folder

---
+++ tensorflow/r2udensenet_1d.py
@@ -0,0 +1,142 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Input, Dropout, Add, Activation, UpSampling2D,  Concatenate
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras import backend as K
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.keras.metrics import Precision, Recall, AUC, Accuracy
+from keras.losses import BinaryCrossentropy
+from metrics import dice_coef,dice_loss,IoU,weighted_bce,composite_loss
+
+##
+import os
+
+K.set_image_data_format('channels_last')
+"""Recurrent Layer"""
+def rec_layer(layer, filters):
+    reconv1 = Conv2D(filters, (3, 3), activation='relu', padding='same')(layer)#(1,32)
+    layer_add = Conv2D(filters, kernel_size=(1, 1), padding='same')(layer) #(1,32)
+    add_conv1 = Add()([reconv1,layer_add]) #(32,32)
+    #drop_inter = Dropout(0.3)(reconc1)
+    reconv1 = Conv2D(filters, (3, 3), activation='relu', padding='same')(add_conv1)#(32,32)
+    add_conv2 = Add()([reconv1,layer_add])#(32,32)
+    reconv1 = Conv2D(filters, (3, 3), activation='relu', padding='same')(add_conv2) #(32,32)
+    return reconv1
+    
+#metrics 
+pr_metric = AUC(curve='PR', num_thresholds=10, name = 'pr_auc') 
+roc_metric = AUC(name = 'auc')
+METRICS = [dice_loss,
+    dice_coef,
+    Precision(name='precision'),
+    Recall(name='recall'),
+    pr_metric,
+    roc_metric,
+    IoU,
+    BinaryCrossentropy(name='binaryCE'),
+    weighted_bce
+]
+
+########## Initialization of Parameters #######################
+# original
+# image_row = 128
+# image_col = 128
+# image_depth = 1
+
+# new
+image_row = 192
+image_col = 192
+image_depth = 1
+
+def r2udensenet():
+    inputs = Input((image_row, image_col, image_depth)) #(1,1)
+    conv1 = rec_layer(inputs,32) #(1,32)
+    conv1 = rec_layer(conv1,32) #(32,32)
+    conv1add = Conv2D(32, kernel_size=(1, 1), padding='same')(inputs) #(1,32)
+    add1 = Add()([conv1add, conv1])#(32,32)
+    dense1 = concatenate([add1, conv1], axis=3) #(32,64)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(dense1)#(64,64)
+
+    conv2 = rec_layer(pool1, 64)
+    conv2 = rec_layer(conv2, 64)
+    conv2add = Conv2D(64, kernel_size=(1, 1), padding='same')(pool1)
+    add2 = Add()([conv2add, conv2])
+    dense2 = concatenate([add2, conv2], axis=3)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(dense2)
+
+    conv3 = rec_layer(pool2, 128)
+    conv3 = rec_layer(conv3, 128)
+    conv3add = Conv2D(128, kernel_size=(1, 1), padding='same')(pool2)
+    add3 = Add()([conv3add, conv3])
+    dense3 = concatenate([add3, conv3], axis=3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(dense3)
+
+    conv4 = rec_layer(pool3, 256)
+    conv4 = rec_layer(conv4, 256)
+    conv4add = Conv2D(256, kernel_size=(1, 1), padding='same')(pool3)
+    add4 = Add()([conv4add, conv4])
+    dense4 = concatenate([add4, conv4], axis=3)
+    drop4 = Dropout(0.5)(dense4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(dense4)
+
+    conv5 = rec_layer(pool4, 512)
+    conv5 = rec_layer(conv5, 512) 
+    conv5add = Conv2D(512, kernel_size=(1, 1), padding='same')(pool4)
+    add5 = Add()([conv5add, conv5]) 
+    dense5 = concatenate([add5, conv5], axis=3) 
+    drop5 = Dropout(0.5)(dense5) 
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(dense5), conv4], axis=3)#(1024,256)/#(256,512)
+    conv6 = rec_layer(up6, 256) 
+    conv6 = rec_layer(conv6, 256) 
+    conv6add = Conv2D(256, kernel_size=(1, 1), padding='same')(up6) #(256,256)
+    add6 = Add()([conv6add, conv6]) 
+    dense6 = concatenate([add6, conv6], axis=3) 
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(dense6), conv3], axis=3)
+    conv7 = rec_layer(up7, 128)
+    conv7 = rec_layer(conv7, 128)
+    conv7add = Conv2D(128, kernel_size=(1, 1), padding='same')(up7)
+    add7 = Add()([conv7add, conv7])
+    dense7 = concatenate([add7, conv7], axis=3)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(dense7), conv2], axis=3)
+    conv8 = rec_layer(up8, 64)
+    conv8 = rec_layer(conv8, 64)
+    conv8add = Conv2D(64, kernel_size=(1, 1), padding='same')(up8)
+    add8 = Add()([conv8add, conv8])
+    dense8 = concatenate([add8, conv8], axis=3)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(dense8), conv1], axis=3)
+    conv9 = rec_layer(up9, 64)
+    conv9 = rec_layer(conv9, 64)
+    conv9add = Conv2D(64, kernel_size=(1, 1), padding='same')(up9)
+    add9 = Add()([conv9add, conv9])
+    dense9 = concatenate([add9, conv9], axis=3)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(dense9)
+    model = Model(inputs=[inputs], outputs=[conv10])
+    #model.summary()
+
+    # model.compile(optimizer=Adam(learning_rate=1e-5), loss= dice_loss, metrics=METRICS)
+    # model.compile(optimizer=Adam(learning_rate=1e-5), loss= 'binary_crossentropy', metrics=METRICS)
+    # dl_weight = K.variable(0.75)
+    # bce_weight = K.variable(0.25)
+    # model.compile(optimizer=Adam(learning_rate=1e-5),
+    #                 loss=[dice_loss, 'binary_crossentropy'],
+    #                 loss_weights=[dl_weight, bce_weight],
+    #                  metrics=METRICS)
+                                                       
+    model.compile(optimizer=Adam(learning_rate=1e-5), loss=composite_loss, metrics=METRICS)
+
+    ### Original is on top
+    pretrained_weights = None
+    # weight_directory = './weights'
+    # pretrained_weights = os.path.join(weight_directory,'model_r2udensenet.hdf5')
+
+    if(pretrained_weights):
+        model.load_weights(pretrained_weights)
+        
+    return model
